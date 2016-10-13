36f59502e903ed31837f2498bddb60edf3e45979
Monika Mociarikova <xmociar@nymfe46.fi.muni.cz>
2016-10-13 13:50:17 +0200

    Nove subory cv3

---
+++ currency-convertor/src/main/java/cz/muni/fi/pa165/currency/MainAnnotations.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.currency;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author xmociar
+ */
+public class MainAnnotations {
+    
+    public static void main(String[] args) {
+        
+        ApplicationContext applicationContext 
+                = new AnnotationConfigApplicationContext("cz.muni.fi.pa165");
+        
+        CurrencyConvertor currencyConvertor = 
+                applicationContext.getBean(CurrencyConvertorImpl.class);
+        
+        
+        currencyConvertor.convert(Currency.getInstance("EUR"),Currency.getInstance("CZK"),BigDecimal.valueOf(1));
+        
+    }
+    
+}
